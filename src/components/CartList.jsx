4bd9692187d4feeb22a8b8f6b558e479cb8ea8c3
res Arfita <hresaarfita@gmail.com>
2024-09-29 22:38:02 +0700

    add button bayar

--- src/components/CartList.jsx
+++ src/components/CartList.jsx
@@ -8,11 +8,11 @@ const CartList = ({ counterKurang, counterTambah }) => {
     return (
         <>
         <div className="">
-            <div className="grid grid-cols-4 ml-20 pt-5">
-                <p className="underline decoration-violet-800">Item</p>
-                <p className="underline decoration-violet-800">Harga</p>
-                <p className="underline decoration-violet-800">Kuantitas</p>
-                <p className="underline decoration-violet-800">Total Harga</p>
+            <div className="grid grid-cols-4 ml-10 gap-2 pt-5">
+                <p className="underline decoration-violet-800 ml-5">Item</p>
+                <p className="underline decoration-violet-800 ml-6">Harga</p>
+                <p className="underline decoration-violet-800 ml-4">Kuantitas</p>
+                <p className="underline decoration-violet-800">H. Total</p>
             </div>
             <DataItems counterKurang={counterKurang} counterTambah={counterTambah} />
         </div>
--- src/components/Counter.jsx
+++ src/components/Counter.jsx
@@ -1,6 +1,6 @@
 /* eslint-disable react/prop-types */
 import { useState, useEffect } from "react"
-import "../components/counter.css"
+//import "../components/counter.css"
 
 const Counter = ({ counterKurang, counterTambah, price }) => {
 
@@ -26,12 +26,19 @@ const Counter = ({ counterKurang, counterTambah, price }) => {
         <>
         <div className="m-10 grid grid-cols-2 justify-between">
             <div>
-                <button onClick={() => {counterKurang(); kurangList()}} className="px-2.5 focus:ring-2 bg-indigo-500 hover:bg-violet-600 text-white">-</button>
+                <button
+                    onClick={() => {counterKurang(); kurangList()}}
+                    className="px-2.5 focus:ring-2 bg-indigo-500 hover:bg-violet-600 text-white">-</button>
                 <span className="px-2">{quantityList}</span>
-                <button onClick={() => {counterTambah(); tambahList()}} className="px-2 focus:ring-2 bg-indigo-500 hover:bg-violet-600 text-white">+</button> 
+                <button
+                    onClick={() => {counterTambah(); tambahList()}}
+                    className="px-2 focus:ring-2 bg-indigo-500 hover:bg-violet-600 text-white">+</button>  
             </div>
-            <div className="ml-20 w-fit">
-                <p className="bg-red-500 harga">$<span className="font-medium">{hargaItem}</span></p>     
+            <div className="grid grid-cols-2 gap-5 justify-end mb-10 mx-10">
+                <p className="harga">$<span className="font-medium">{hargaItem}</span></p>
+                <div className="btn2">
+                    <button className="underline decoration-green-800"><span className="bayar">Bayar</span></button>
+                </div>   
             </div>
             
         </div>
