6e98f43d46eb289268985eef926edc6fff0ca51c
res Arfita <hresaarfita@gmail.com>
2024-09-29 13:38:20 +0700

    Update harga tiap kali kuantitas berubah dengan UseEffect

--- src/components/Counter.jsx
+++ src/components/Counter.jsx
@@ -1,25 +1,38 @@
 /* eslint-disable react/prop-types */
-import { useState } from "react"
+import { useState, useEffect } from "react"
 
-const Counter = ({ counterKurang, counterTambah }) => {
+const Counter = ({ counterKurang, counterTambah, price }) => {
 
     //state untuk kuantitas didalam list
     const [quantityList, setQuantityList] = useState(1)
 
     const kurangList = () => {
         quantityList > 1 ? setQuantityList(quantityList - 1) : quantityList
-      }
+    }
     
     const tambahList = () => {
         setQuantityList(quantityList + 1)
     }
 
+    //state harga
+    const [hargaItem, setHargaItem] = useState(price)
+    //update harga tiap kali quantity/kuantitas item berubah
+    useEffect(() => {
+        setHargaItem(quantityList * price)
+    }, [quantityList])
+
     return (
         <>
-        <div className="m-10">
-            <button onClick={() => {counterKurang(); kurangList()}} className="px-2.5 focus:ring-2">-</button>
-            <span className="px-2">{quantityList}</span>
-            <button onClick={() => {counterTambah(); tambahList()}} className="px-2 focus:ring-2">+</button>    
+        <div className="m-10 grid grid-cols-2 justify-between">
+            <div>
+                <button onClick={() => {counterKurang(); kurangList()}} className="px-2.5 focus:ring-2 bg-indigo-500 hover:bg-violet-600 text-white">-</button>
+                <span className="px-2">{quantityList}</span>
+                <button onClick={() => {counterTambah(); tambahList()}} className="px-2 focus:ring-2 bg-indigo-500 hover:bg-violet-600 text-white">+</button> 
+            </div>
+            <div className="ml-20 w-fit">
+                <p className="ml-20">$<span className="font-medium">{hargaItem}</span></p>     
+            </div>
+            
         </div>
         </>
     )
